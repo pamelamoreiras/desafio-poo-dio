de852c9e4a3d3562d8ad52d5ac9b492a035d1936
Pamela Moreira <pamelamoreiras99@gmail.com>
2022-10-05 16:19:27 -0300

    refatorando curso e mentoria e classe conteúdo criada

--- src/main/java/br/com/dio/desafio/Main.java
+++ src/main/java/br/com/dio/desafio/Main.java
@@ -1,7 +1,30 @@
 package br.com.dio.desafio;
 
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Curso curso1 = new Curso();
+        curso1.setTitulo("curso java");
+        curso1.setDescricao("curso expert java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("curso javascript");
+        curso2.setDescricao("curso expert javascript");
+        curso2.setCargaHoraria(8);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("mentoria de java");
+        mentoria.setDescricao("mentoria de spring framework");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
     }
 }
--- src/main/java/br/com/dio/desafio/dominio/Curso.java
+++ src/main/java/br/com/dio/desafio/dominio/Curso.java
@@ -1,34 +1,10 @@
 package br.com.dio.desafio.dominio;
 
-public class Curso {
+public class Curso extends Conteudo{
 
-    private String titulo;
-    private String descricao;
     private Integer cargaHoraria;
 
     public Curso() {
-    }
-
-    public Curso(String titulo, String descricao, Integer cargaHoraria) {
-        this.titulo = titulo;
-        this.descricao = descricao;
-        this.cargaHoraria = cargaHoraria;
-    }
-
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
     }
 
     public Integer getCargaHoraria() {
@@ -42,9 +18,14 @@ public class Curso {
     @Override
     public String toString() {
         return "Curso{" +
-                "titulo='" + titulo + '\'' +
-                ", descricao='" + descricao + '\'' +
+                "titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
                 ", cargaHoraria=" + cargaHoraria +
                 '}';
+    }
+
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO * cargaHoraria;
     }
 }
